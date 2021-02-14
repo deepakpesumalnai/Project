06c7a38edf8bc0294d4de95f6a7326be7ed1abcd
deepak <deepakpesu@gmail.com>
2021-02-14 21:08:19 +0530

    sample interitance

--- Sample Programs/Class/inheritance.py
+++ Sample Programs/Class/inheritance.py
@@ -1,5 +1,7 @@
 from Simpleclass import Person
 
+print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__, __name__, str(__package__)))
+
 class man(Person):
 	def test(self):
 		print('child')
